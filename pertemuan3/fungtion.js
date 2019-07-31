fd9db16734e76302b69ad48d46f6013891e56afb
endah22 <purwatiendah22@gmail.com>
2019-07-31 12:28:38 +0700

    learn function

---
+++ pertemuan3/fungtion.js
@@ -0,0 +1,19 @@
+// Function Expression
+function greet(){
+  consile.log('Hello World')
+}
+
+function alertUser() {
+  console.log('Error Detected')
+
+}
+
+greet()
+alertUser()
+
+//function parameter 
+function greet(name){
+  console.log(`hello ${name}`)
+}
+greet('endah')
+greet('dwi')
---
+++ pertemuan3/latihan.js
@@ -0,0 +1,7 @@
+function angka(i,f){
+  console.log(i +  f)
+
+}
+
+angka (2,6)
+angka (4,6)
